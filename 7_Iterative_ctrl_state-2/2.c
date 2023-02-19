66e69d0798645c79cc33b8034964741c80f754cb
7388505950 <112402883+7388505950@users.noreply.github.com>
2023-02-19 11:50:17 +0530

    Add files via upload

--- 7_Iterative_ctrl_state-2/2.c
+++ 7_Iterative_ctrl_state-2/2.c
@@ -1,1 +1,15 @@
-
+#include<stdio.h>//caiculate the sum of cube n natural number:
+#include<conio.h>
+int main()
+{
+    int a=0,b=1,c,n,i;
+     printf("enter any number\n");
+     scanf("%d",&a);
+     for(i=1;i<=n;i++)
+     {
+        printf("%d",a);
+        c=a+b;
+        a=b;
+        b=c;
+     }
+}
