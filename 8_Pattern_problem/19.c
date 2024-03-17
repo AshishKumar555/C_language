49f01969bad28e0d623de5ca6a51d3dea3235ca3
AshishKumar555 <112402883+AshishKumar555@users.noreply.github.com>
2024-03-17 21:52:24 +0530

    Heart updated with Ashish

--- 8_Pattern_problem/19.c
+++ 8_Pattern_problem/19.c
@@ -1,33 +1,35 @@
-#include<stdio.h>
-#include<conio.h>
-int main()
-{
-    int i,j;
-    
-    for(i=1;i<=3;i++)
-    {
-       
-        for(j=1;j<=19;j++)
-        {
-        if( (j>=4-i&&j<=6+i)||(j>=14-i&&j<=16+i))
-              
-        printf("N");
-        else
-        printf(" ");
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=1;i<=3;i++){
+        for(int j=1;j<=19;j++){
+            if((j>=2-i && j<=5+i)||(j>=14-i && j<=16+i)){
+                cout<<"*";
+            }else{
+                cout<<" ";
+            }
         }
-        printf("\n");
+        cout<<endl;
     }
-    for(i=1;i<=10;i++)
-    {
-        for(j=1;j<=19;j++)
-        {
-        if(j>=i&&j<=20-i)
-        printf("N");
-        else
-        printf(" ");
+    for(int i=1;i<=1;i++){
+        for(int j=1;j<=12;j++){
+            if(j==6){
+                cout<<" Ashish ";
+            }else{
+                cout<<"*";
+            }
+        }
+        cout<<endl;
+    }
 
+    for(int i=1;i<=9;i++){
+        for(int j=1;j<=19;j++){
+            if(j>=i+1 &&j<20-i){
+               cout<<"*";
+            }else{
+                cout<<" ";
+            }
         }
-        printf("\n");
+        cout<<endl;
     }
-    return 0;
 }
