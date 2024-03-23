061520f7d01d1d15689c92fa35bb65566cf6d7af
AshishKumar555 <112402883+AshishKumar555@users.noreply.github.com>
2024-03-23 14:43:35 +0530

    Update calNnatu.c

--- 0.06Loop/calNnatu.c
+++ 0.06Loop/calNnatu.c
@@ -1,15 +1,1 @@
 
-#include<stdio.h>//caiculate the sum of n natural number:
-#include<conio.h>
-int main()
-{
-    int i,n,s=0;
-    printf("Enter any number\n");
-    scanf("%d",&n);
-    for(i=1;i<=n;i++)
-    s=s+i;
-    printf("The sum of %d natural no is=%d\n",n,s);
-
-    return 0;
-
-}
