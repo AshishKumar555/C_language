f29964d67e139ce9bbeaf64499f1cadd40dd6b1e
AshishKumar555 <112402883+AshishKumar555@users.noreply.github.com>
2024-03-27 16:48:36 +0530

    Update 4.c

--- 9.20_POinter/4.c
+++ 9.20_POinter/4.c
@@ -1,1 +1,15 @@
-
+#include<stdio.h>
+int main()
+{
+    int x=10,*p,**q,***r;
+    p=&x;
+    q=&p;
+    r=&q;
+    
+    printf("%d %d %d %d\n",x,*p,**q,***r);
+    printf("%d %d %d %d \n",&x,p,*q,**r);
+    printf("%d %d  %d\n",&p,q,*r);
+    printf("%d %d \n",&q,r);
+    printf("%d\n",&r);
+    return 0;
+}
