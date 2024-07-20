05b812889b20c4d204bbff1ef1205f8b682118bf
Ashish Kumar Maurya <112402883+AshishKumar555@users.noreply.github.com>
2024-07-20 20:22:30 +0530

    cpp me update h

--- 9.18String_Function/6.C
+++ 9.18String_Function/6.C
@@ -1,20 +1,37 @@
 #include<stdio.h>
-void alphanumeric(char s[20]);
+#include<iostream>
+using namespace std;
+int alphanumeric(char s[20]);
 int main()
 {
     int i;
     char s[20];
     printf("enter the no of string\n");
     fgets(s,20,stdin);
-    alphanumeric(s);
+   
+    if(alphanumeric(s)){
+        cout<<"The string is Alphanumeric :"<<endl;
+    }
+    else{
+        cout<<"The string is not Alphanumeric "<<endl;
+    }
     return 0;
 }
-void alphanumeric(char s[20])
+int alphanumeric(char s[20])
 {
-    int i;
+    int i,vowel=0,digit=0;
     for(i=0;s[i];i++)
-    if(s[i]>='a'&&s[i]<='z'||s[i>='1'&&s[i]<=9])
-        printf("Alphanumeric\n");
-    else
-    printf("not alphanumeric\n");
-}uyffouy
+    {
+    if((s[i]>='a'&& s[i]<='z')||(s[i]>='A'&& s[i]<='Z'))        
+        vowel++;    
+    else if( s[i]>='0' && s[i]<='9' )
+        digit++;
+    }
+     cout<<vowel<<' '<<digit<<" ";
+    if(vowel >=1 && digit >=1){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
