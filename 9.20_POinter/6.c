5609c2c490095d00e4b34f9ae6f18a83b62feab3
Ashish Kumar Maurya <112402883+AshishKumar555@users.noreply.github.com>
2024-09-07 20:35:00 +0530

    Update 6.c

---
+++ 9.20_POinter/6.c
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char *ptr="Ashish";
+    int length=0;
+    for(int i=0;i<=*(ptr+i);i++){
+        length++;
+    }
+    cout<<"Length is => "<<length;
+    return 0;
+}
